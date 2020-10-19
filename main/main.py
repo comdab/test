6b744e6a955b9482e07922229e3b9ffb2b711301
comdab <72580186+comdab@users.noreply.github.com>
2020-10-19 20:09:53 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,91 +1,25 @@
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
-from kivy.core.text import LabelBase
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
-from kivy.uix.behaviors import ButtonBehavior
-from kivy.uix.label import Label
-from kivy.config import Config
-from kivy.uix.image import Image
-from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle, Color
-from kivy.graphics.vertex_instructions import RoundedRectangle
+....
+    w = ButtonList(id_button=str(id_resume))
+    # w.bind(on_release=lambda x: self.show_detail(x.id_button))    
+    # w.fbind('on_release', self.show_detail)
+    w.bind(on_release=self.show_detail)
+    self.listing.add_widget(w)
+...
 
-Config.set('graphics', 'width', '320')
-Config.set('graphics', 'height', '580')
+def show_detail(self, item):
+    self.save_selection(item.id_button)
+    sm.transition = SlideTransition()
+    sm.current = "item_detail"
+    sm.transition.direction = "left"
+...
 
-LabelBase.register(name="SinkinSans",
-                   fn_regular="police\SinkinSans-400Regular.otf",
-                   fn_bold="police\SinkinSans-700Bold.otf")
+class ButtonList(BoxLayout, Button):
 
-LabelBase.register(name="SourceSansPro",
-                   fn_regular="police\SourceSansPro-Regular.otf",
-                   fn_bold="police\SourceSansPro-Bold.otf")
+    def __init__(self, **kwargs):
+        super().__init__()
+        with self.canvas.before:
+            Color(0, 0, 0.7, 1, mode='rgba') if self.state == 'normal' else Color(0, 0, 1, 1, mode='rgba')
+            self.rect = RoundedRectangle(size=self.size, pos=self.pos, radius=[(10, 10), (10, 10), (10, 10), (10, 10)])
+        self.bind(pos=self.update_rect, size=self.update_rect)
 
-
-class PublicHelp(Screen):
-    pass
-
-
-class PublicLanding(Screen):
-    pass
-
-
-class PublicSearch(Screen):
-    pass
-
-
-class PublicResult(Screen):
-    pass
-
-
-class Result(Screen):
-    pass
-
-
-class Help(Screen):
-    pass
-
-
-class Login(Screen):
-    pass
-
-
-class Account(Screen):
-    pass
-
-
-class Search(Screen):
-    pass
-
-
-class Landing(Screen):
-    pass
-
-
-class WindowManager(ScreenManager):
-    pass
-
-
-kv = Builder.load_file("my.kv")
-
-
-class MyMainApp(App):
-    def build(self):
-        return kv
-
-    def on_start(self):
-        advertise_list = self.root.ids['result'].ids['advertise_list']
-        for i in range(10):
-            w = Button(text="Advertise here ! dmiqh dhfiodms id os qhiod q id ")
-            advertise_list.add_widget(w)
-
-        advertise_public_list = self.root.ids['public_result'].ids['advertise_list']
-        for i in range(10):
-            x = Button(text="Advertise here !")
-            advertise_public_list.add_widget(x)
-
-
-app = MyMainApp()
-app.run()
+        self.id_button = kwargs['id_button']
